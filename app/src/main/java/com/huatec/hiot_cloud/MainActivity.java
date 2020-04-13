60fa89c7f8818f3f26e1c2778deefdeca4468e33
Ro <760423530@qq.com>
2020-04-13 18:43:55 +0800

    修改日志

--- app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -3,6 +3,7 @@ package com.huatec.hiot_cloud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("log","msg");
 
     }
 
